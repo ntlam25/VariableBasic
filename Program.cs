8312e94f09a026343c7c554b6f3b3c4caaaf0b5d
Tung Lam <lam2k3d@gmail.com>
2025-01-24 10:45:53 +0700

    Create new Student in Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using VariableBasic.Models;
+
 Console.WriteLine("Hello, Hi!");
+
+var lam = new Student();
+lam.Id = 1;
+lam.Name = "Tung Lam";
+lam.Email = "lam2k3d@gmail.com";
+System.Console.WriteLine(lam.Id);
+System.Console.WriteLine(lam.Name);
+System.Console.WriteLine(lam.Email);
