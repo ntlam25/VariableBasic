27ae8760c3fecfcdac4439e1b2ad2bdd6bf229f0
Tung Lam <lam2k3d@gmail.com>
2025-01-24 10:31:20 +0700

    Create Student model

---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace VariableBasic.Models;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
